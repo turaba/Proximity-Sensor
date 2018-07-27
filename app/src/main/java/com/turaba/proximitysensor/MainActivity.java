40a2dbd4c3f45d456ebaeea44c6193527f7bf256
Mahbuba Yesmin <mahbba.yesmin@gmail.com>
2018-07-28 02:08:57 +0600

    init commit

---
+++ app/src/main/java/com/turaba/proximitysensor/MainActivity.java
@@ -0,0 +1,57 @@
+package com.turaba.proximitysensor;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sensorManager;
+    TextView tvProximityValue;
+    private Sensor mProximity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvProximityValue=findViewById(R.id.tv_proximity);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        mProximity=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+    public void onResume() {
+
+        super.onResume();
+
+        sensorManager.registerListener(this,mProximity,2*1000*1000);
+    }
+
+    public void onPause() {
+
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.values[0] < mProximity.getMaximumRange()) {
+            // Detected something nearby
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+
+        } else {
+            // Nothing is nearby
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
